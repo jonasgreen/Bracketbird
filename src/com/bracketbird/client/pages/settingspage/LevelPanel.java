e4575c6f2e543a36e59617a1e62270afc28195d2
Jonas Green <jg@schantz.com>
2014-10-12 22:18:50 +0200

    popup for add stage

--- src/com/bracketbird/client/pages/settingspage/LevelPanel.java
+++ src/com/bracketbird/client/pages/settingspage/LevelPanel.java
@@ -2,8 +2,13 @@ package com.bracketbird.client.pages.settingspage;
 
 import com.bracketbird.client.Flex;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.PopupPanel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +20,8 @@ public class LevelPanel extends FlowPanel {
 
 
     private List<LevelComponent> levelComponents = new ArrayList<LevelComponent>();
+    private AddStageDropDown popup;
+
 
     private Button button;
 
@@ -40,23 +47,40 @@ public class LevelPanel extends FlowPanel {
         if (button == null) {
             button = new Button("Add Stage/Level");
             button.setStyleName("primaryButton");
-
             button.getElement().getStyle().setMarginLeft(40, Style.Unit.PX);
+            button.addClickHandler(new ClickHandler() {
+                @Override
+                public void onClick(ClickEvent event) {
+                    if (popup == null) {
+                        popup = new AddStageDropDown();
+                        popup.setWidth(button.getOffsetWidth() + "px");
+                        popup.show();
+                        popup.getElement().getStyle().setTop(button.getAbsoluteTop() + button.getOffsetHeight(), Style.Unit.PX);
+                        popup.getElement().getStyle().setLeft(button.getAbsoluteLeft(), Style.Unit.PX);
+                        popup.addCloseHandler(new CloseHandler<PopupPanel>() {
+                            @Override
+                            public void onClose(CloseEvent<PopupPanel> event) {
+                                popup = null;
+                            }
+                        });
+
+                        popup.addAutoHidePartner(button.getElement());
+                    }
+                    else{
+                        popup.hide();
+                    }
+                }
+            });
+
+
 
         }
         return button;
     }
 
-    private boolean isEmpty(){
+    private boolean isEmpty() {
         return levelComponents.size() == 1 && levelComponents.get(0) instanceof AllEmptyLevelComponent;
     }
-
-
-
-
-
-
-
 
 
 }
